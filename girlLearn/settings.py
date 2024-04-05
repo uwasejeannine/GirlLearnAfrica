498831a080c33c68c84e779c20cc5200496cec72
uwasejeannine <jeannineuwasee@gmail.com>
2024-04-05 22:02:23 +0200

    add changes

--- girlLearn/settings.py
+++ girlLearn/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3b6ov!2g7$y@(x=@!#v-ik=kh4=*4xol3wk841@gtd&p9edxgd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
